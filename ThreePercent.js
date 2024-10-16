f1ac9f97020c2fd411e9861a321ce83f13ca836b
GHSvensson <gustavthesvensson@gmail.com>
2024-10-16 20:44:51 +0200

    Update ThreePercent.js

--- ThreePercent.js
+++ ThreePercent.js
@@ -2,7 +2,7 @@ function submitValue() {
     const initialValue = parseFloat(document.getElementById('initialValue').value);
     
     if (isNaN(initialValue) || initialValue <= 0) {
-        alert('Please enter a valid number greater than 0');
+        alert('Ange nummer högre än 10');
         return;
     }
 
